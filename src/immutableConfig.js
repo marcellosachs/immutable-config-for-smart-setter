248bd66a5aa91ab7e232639b43f5a431ae24de81
Marcello Sachs <marcellosachs@Marcellos-MBP.jetpack>
2016-09-16 17:14:42 -0400

    immutable functions

---
+++ src/immutableConfig.js
@@ -0,0 +1,27 @@
+import Im from 'immutable';
+
+const immutableConfig = {
+  get: ($$obj, propKey) => {
+    return $$obj.get(propKey)
+  },
+  set: ($$obj, propKey, value) => {
+    return $$obj.set(propKey, value)
+  },
+  getKeys: $$obj => {
+    return $$obj.keySeq().toArray()
+  },
+  isObjectOrMap: $$item => {
+    return Im.Map.isMap(item)
+  },
+  isArrayOrList: $$item => {
+    return Im.List.isList($$item)
+  },
+  push: ($$arr, $$item) => {
+    return $$arr.push($$item)
+  },
+  clone: $$obj => {
+    return $$obj // no need to clone since object is immutable
+  },
+}
+
+export default immutableConfig
